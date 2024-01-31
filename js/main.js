0a1d4c51c6fb4908fa6df6617a6807d9b8a9bc33
ramziddin12345 <r03571832@gmail.com>
2024-01-31 20:06:37 +0500

    igjnbijgmb

---
+++ js/main.js
@@ -0,0 +1,10 @@
+let a = prompt ("Ismingizni kiriting:")
+let b = prompt ("Familiyangizni kiriting:")
+let c = prompt ("Yoshingizni kiriting:")
+let d = prompt ("Qaysi kursda o'qimoqchisiz")
+let l = confirm ("Ma'lumotlaringizni hammasi tog'rimi? ")
+let u = "5. Sizning id raqamingiz" 
+if(a){
+    alert("1. "+a+"\n"+"2. "+b+"\n3. "+c+"\n4. "+d+"\n"+u+" "+(Math.random()).toFixed(5) )
+    
+}
